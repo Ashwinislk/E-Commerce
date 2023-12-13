845b60c17a401a60ee25a48d2d7dcab8c9e08118
Ashwinislk <ashwinislk391@gmail.com>
2023-12-13 16:18:10 +0530

    Logger and documentation of product module completed

--- src/main/java/com/electronicstore/controller/ProductController.java
+++ src/main/java/com/electronicstore/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.electronicstore.payload.ApiResponse;
 import com.electronicstore.payload.PageableResponse;
 import com.electronicstore.payload.ProductDto;
 import com.electronicstore.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,58 +13,128 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
+@Slf4j
 public class ProductController {
 
     @Autowired
     private ProductService productService;
 
+    /**
+     * @author Ashwini Shelke
+     * @apiNote save product records
+     * @param productDto
+     * @return
+     * @since 1.0 v
+     */
     @PostMapping("/products")
     public ResponseEntity<ProductDto> createProduct(@RequestBody ProductDto productDto) {
+        log.info("Entering the request for save product data");
         ProductDto productDto1 = this.productService.saveProduct(productDto);
+        log.info("Completed the request for save product data");
         return new ResponseEntity<ProductDto>(productDto1, HttpStatus.CREATED);
     }
 
+    /**
+     * @author Ashwini Shelke
+     * @apiNote update product data
+     * @param productDto
+     * @param productId
+     * @return
+     * @since 1.0v
+     */
     @PutMapping("/products/update/productId/{productId}")
     public ResponseEntity<ProductDto> updateProduct(@RequestBody ProductDto productDto, @PathVariable String productId) {
+        log.info("Entering the request for update product data with productId:{}",productId);
         ProductDto productDto1 = this.productService.updateProduct(productDto, productId);
+        log.info("Completed the request for update product data with ProductId:{}",productId);
         return new ResponseEntity<>(productDto1, HttpStatus.OK);
     }
 
+    /**
+     * @author Ashwini Shelke
+     * @apiNote get single product with id
+     * @param productId
+     * @return
+     * @since 1.0v
+     */
     @GetMapping("/products/productId/{productId}")
     public ResponseEntity<ProductDto> getSingleProduct(@PathVariable String productId) {
+        log.info("Entering the request for get single product with productId:{}",productId);
         ProductDto singleProduct = this.productService.getSingleProduct(productId);
+        log.info("Completed the request for get single product with productId:{}",productId);
         return new ResponseEntity<>(singleProduct, HttpStatus.OK);
     }
 
+    /**
+     * @author Ashwini Shelke
+     * @apiNote get all products data
+     * @param pageNumber
+     * @param pageSize
+     * @param sortBy
+     * @param sortDir
+     * @return
+     * @since 1.0v
+     */
     @GetMapping("/products")
     public ResponseEntity<PageableResponse> getAllProducts(
             @RequestParam(value = "pageNumber", defaultValue = AppConstant.PAGE_NUMBER, required = false) Integer pageNumber,
             @RequestParam(value = "pageSize", defaultValue = AppConstant.PAGE_SIZE, required = false) Integer pageSize,
             @RequestParam(value = "sortBy", defaultValue = AppConstant.PRODUCTS_SORT_BY, required = false) String sortBy,
             @RequestParam(value = "sortDir", defaultValue = AppConstant.SORT_DIR, required = false) String sortDir) {
-
+        log.info("Entering the request for get all product data");
         PageableResponse<ProductDto> allProducts = this.productService.getAllProducts(pageNumber, pageSize, sortBy, sortDir);
+        log.info("Completed the request for get all product data");
         return new ResponseEntity<>(allProducts, HttpStatus.OK);
     }
 
+    /**
+     * @author Ashwini Shelke
+     * @apiNote delete product with id
+     * @param productId
+     * @return
+     * @since 1.0v
+     */
     @DeleteMapping("/products/productId/{productId}")
     public ResponseEntity<ApiResponse> deleteProduct(@PathVariable String productId) {
+        log.info("Entering the request for delete single product data with productId:{}",productId);
         ApiResponse response = ApiResponse.builder().message(AppConstant.DELETE).status(false).httpcode(HttpStatus.OK).build();
         this.productService.deleteProduct(productId);
-
+        log.info("Completed the request for delete single product data with productId:{}",productId);
         return new ResponseEntity<ApiResponse>(response, HttpStatus.OK);
     }
 
+    /**
+     * @author Ashwini Shelke
+     * @apiNote get by live true
+     * @param pageNumber
+     * @param pageSize
+     * @param sortBy
+     * @param sortDir
+     * @return
+     * @since 1.0v
+     */
     @GetMapping("/products/livetrue")
     public ResponseEntity<PageableResponse> getByLiveTrue(
             @RequestParam(value = "pageNumber", defaultValue = AppConstant.PAGE_NUMBER, required = false) Integer pageNumber,
             @RequestParam(value = "pageSize", defaultValue = AppConstant.PAGE_SIZE, required = false) Integer pageSize,
             @RequestParam(value = "sortBy", defaultValue = AppConstant.PRODUCTS_SORT_BY, required = false) String sortBy,
             @RequestParam(value = "sortDir", defaultValue = AppConstant.SORT_DIR, required = false) String sortDir) {
+        log.info("Entering the request for get by live true with pagination");
         PageableResponse<ProductDto> byLiveTrue = this.productService.findByLiveTrue(pageNumber, pageSize, sortBy, sortDir);
+        log.info("Entering the request for get by live true with pagination");
         return new ResponseEntity<>(byLiveTrue, HttpStatus.OK);
     }
 
+    /**
+     * @author Ashwini Shelke
+     * @apiNote get all live products
+     * @param pageNumber
+     * @param pageSize
+     * @param sortBy
+     * @param sortDir
+     * @return
+     * @since 1.0v
+     */
     @GetMapping("/products/getAllLiveProducts")
     public ResponseEntity<PageableResponse> getAllLiveProducts(
             @RequestParam(value = "pageNumber", defaultValue = AppConstant.PAGE_NUMBER, required = false) Integer pageNumber,
@@ -71,42 +142,90 @@ public class ProductController {
             @RequestParam(value = "sortBy", defaultValue = AppConstant.PRODUCTS_SORT_BY, required = false) String sortBy,
             @RequestParam(value = "sortDir", defaultValue = AppConstant.SORT_DIR, required = false) String sortDir) {
 
+        log.info("Entering the request for get all live products with pagination");
         PageableResponse<ProductDto> allLiveProducts = this.productService.getAllLiveProducts(pageNumber, pageSize, sortBy, sortDir);
+        log.info("Completed the request for get all live products with pagination");
         return new ResponseEntity<>(allLiveProducts, HttpStatus.OK);
     }
 
+    /**
+     * @author Ashwini Shelke
+     * @apiNote get by product bt title
+     * @param pageNumber
+     * @param pageSize
+     * @param sortBy
+     * @param sortDir
+     * @param keyword
+     * @return
+     * @since 1.0v
+     */
     @GetMapping("/products/productsByTitle/{keyword}")
     public ResponseEntity<PageableResponse> getByProductsByTitle(
             @RequestParam(value = "pageNumber", defaultValue = AppConstant.PAGE_NUMBER, required = false) Integer pageNumber,
             @RequestParam(value = "pageSize", defaultValue = AppConstant.PAGE_SIZE, required = false) Integer pageSize,
             @RequestParam(value = "sortBy", defaultValue = AppConstant.PRODUCTS_SORT_BY, required = false) String sortBy,
             @RequestParam(value = "sortDir", defaultValue = AppConstant.SORT_DIR, required = false) String sortDir, @PathVariable String keyword) {
+        log.info("Entering the request for get by products by title with pagination");
         PageableResponse<ProductDto> productsByTitle = this.productService.findProductsByTitle(pageNumber,pageSize,sortBy, sortDir,keyword);
+        log.info("Completed the request for get by products by title with pagination");
         return new ResponseEntity<>(productsByTitle, HttpStatus.OK);
 
     }
+
+    /**
+     * @author Ashwini Shelke
+     * @apiNote save product with category
+     * @param categoryId
+     * @param productDto
+     * @return
+     * @since 1.0v
+     */
    @PostMapping("/category/{categoryId}")
     public ResponseEntity<ProductDto> saveProductWithCategoryId(@PathVariable String categoryId,@RequestBody ProductDto productDto){
-
+       log.info("Entering the request for save products with category with categoryId:{}",categoryId);
         ProductDto product = this.productService.createProductWithCategory(productDto, categoryId);
+       log.info("Completed the request for save products with category with categoryId:{}",categoryId);
         return new ResponseEntity<>(product,HttpStatus.CREATED);
 
     }
 
+    /**
+     * @author Ashwini Shelke
+     * @apiNote  get all users category
+     * @param categoryId
+     * @param pageNumber
+     * @param pageSize
+     * @param sortBy
+     * @param direction
+     * @return
+     * @since 1.0v
+     */
     @GetMapping("/category/{categoryId}")
-    public ResponseEntity<PageableResponse<ProductDto>> getAllUsersCategory(
+    public ResponseEntity<PageableResponse<ProductDto>> getAllProductsWithCategory(
             @PathVariable String categoryId,
             @RequestParam(value = "pageNumber", defaultValue = AppConstant.PAGE_NUMBER, required = false) Integer pageNumber,
             @RequestParam(value = "pageSize", defaultValue = AppConstant.PAGE_SIZE, required = false) Integer pageSize,
             @RequestParam(value = "sortBy", defaultValue = AppConstant.PRODUCTS_SORT_BY, required = false) String sortBy,
             @RequestParam(value = "direction", defaultValue = AppConstant.SORT_DIR, required = false) String direction   ){
-
+        log.info("Entering the request for get all products category with categoryId:{}",categoryId);
         PageableResponse<ProductDto> allOfCategory = this.productService.getAllOfCategory(categoryId, pageNumber, pageSize, sortBy, direction);
+        log.info("Completed the request for get all products category with categoryId:{}",categoryId);
         return new ResponseEntity<>(allOfCategory,HttpStatus.OK);
     }
+
+    /**
+     * @author Ashwini Shelke
+     * @apiNote update product with category
+     * @param productId
+     * @param categoryId
+     * @return
+     * @since 1.0v
+     */
     @PutMapping("/products/{productId}/category/{categoryId}")
     public ResponseEntity<ProductDto> updateProductWithCategory(@PathVariable String productId,@PathVariable String categoryId){
+        log.info("Entering the request for update product with category with categoryId and productId:{}:{}",categoryId,productId);
         ProductDto updateCategory = this.productService.updateCategory(categoryId, productId);
+        log.info("Completed the request for update product with category with categoryId and productId:{}:{}",categoryId,productId);
         return new ResponseEntity<>(updateCategory,HttpStatus.OK);
 
     }
--- src/main/java/com/electronicstore/service/Impl/ProductServiceImpl.java
+++ src/main/java/com/electronicstore/service/Impl/ProductServiceImpl.java
@@ -10,6 +10,7 @@ import com.electronicstore.payload.ProductDto;
 import com.electronicstore.repository.CategoryRepository;
 import com.electronicstore.repository.ProductRepository;
 import com.electronicstore.service.ProductService;
+import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -21,6 +22,7 @@ import java.util.Date;
 import java.util.UUID;
 
 @Service
+@Slf4j
 public class ProductServiceImpl implements ProductService {
 
     @Autowired
@@ -34,6 +36,7 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public ProductDto saveProduct(ProductDto productDto) {
+        log.info("Entering the dao call for create product data");
         Product product = this.modelMapper.map(productDto, Product.class);
         Date date = new Date();
         String id = UUID.randomUUID().toString();
@@ -41,18 +44,21 @@ public class ProductServiceImpl implements ProductService {
         product.setAddedDate(date);
 
         Product newproduct = this.productRepository.save(product);
+        log.info("Completed the dao call for create product data");
         return modelMapper.map(newproduct, ProductDto.class);
     }
 
     @Override
     public ProductDto getSingleProduct(String productId) {
+        log.info("Entering the dao call for get single product data with productId:{}",productId);
         Product product = this.productRepository.findById(productId).orElseThrow(() -> new ResourceNotFound(AppConstant.NOT_FOUND + productId));
+        log.info("Completed the dao call for get single product data with productId:{}",productId);
         return modelMapper.map(product, ProductDto.class);
     }
 
     @Override
     public ProductDto updateProduct(ProductDto productDto, String productId) {
-
+        log.info("Entering the dao call for update product data with productId:{}",productId);
         Product product = this.productRepository.findById(productId).orElseThrow(() -> new ResourceNotFound(AppConstant.NOT_FOUND + productId));
         product.setDescription(productDto.getDescription());
         product.setTitle(productDto.getTitle());
@@ -62,29 +68,33 @@ public class ProductServiceImpl implements ProductService {
         product.setLive(productDto.getLive());
         product.setStock(productDto.getStock());
         Product saveProduct = this.productRepository.save(product);
+        log.info("Completed the dao call for update product data with productId:{}",productId);
         return modelMapper.map(saveProduct, ProductDto.class);
     }
 
     @Override
-    public void deleteProduct(String prodyctId) {
-
-        Product product = this.productRepository.findById(prodyctId).orElseThrow(() -> new ResourceNotFound(AppConstant.NOT_FOUND + prodyctId));
+    public void deleteProduct(String productId) {
+        log.info("Entering the dao call for delete product data with productId:{}",productId);
+        Product product = this.productRepository.findById(productId).orElseThrow(() -> new ResourceNotFound(AppConstant.NOT_FOUND + productId));
         productRepository.delete(product);
-
+        log.info("Completed the dao call for delete product data with productId:{}",productId);
     }
 
     @Override
     public PageableResponse<ProductDto> getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String direction) {
+        log.info("Entering the dao call for get all products with the pagination");
         Sort sort = (direction.equalsIgnoreCase("desc")) ? (Sort.by(sortBy).descending()) : (Sort.by(sortBy).ascending());
 
         PageRequest pages = PageRequest.of(pageNumber, pageSize, sort);
         Page<Product> all = productRepository.findAll(pages);
         PageableResponse<ProductDto> pageableResponse = Helper.getPageableResponse(all, ProductDto.class);
+        log.info("Completed the dao call for get all products with the pagination");
         return pageableResponse;
     }
 
     @Override
     public PageableResponse<ProductDto> findByLiveTrue(Integer pageNumber, Integer pageSize, String sortBy, String direction) {
+        log.info("Entering the dao call for find by live true with the pagination");
         Sort sort;
         if (direction.equalsIgnoreCase("asc")) {
             sort = Sort.by(sortBy).ascending();
@@ -95,31 +105,37 @@ public class ProductServiceImpl implements ProductService {
         PageRequest pages = PageRequest.of(pageNumber, pageSize, sort);
         Page<Product> byLiveTrue = productRepository.findByLiveTrue(pages);
         PageableResponse<ProductDto> pageableResponse = Helper.getPageableResponse(byLiveTrue, ProductDto.class);
+        log.info("Completed the dao call for find by live true with the pagination");
         return pageableResponse;
     }
 
     @Override
     public PageableResponse<ProductDto> getAllLiveProducts(Integer pageNumber, Integer pageSize, String sortBy, String direction) {
+        log.info("Entering the dao call for get all live products with the pagination");
         Sort sort = (direction.equalsIgnoreCase("desc")) ? (Sort.by(sortBy).descending()) : (Sort.by(sortBy).ascending());
 
         PageRequest pages = PageRequest.of(pageNumber, pageSize, sort);
         Page<Product> products = productRepository.findAll(pages);
         PageableResponse<ProductDto> pageableResponse = Helper.getPageableResponse(products, ProductDto.class);
+        log.info("Completed the dao call for get all live products with the pagination");
         return pageableResponse;
     }
 
     @Override
     public PageableResponse<ProductDto> findProductsByTitle(Integer pageNumber, Integer pageSize, String sortBy, String direction,String keyword) {
+        log.info("Entering the dao call for find product by title with the pagination");
         Sort sort = (direction.equalsIgnoreCase("desc")) ? (Sort.by(sortBy).descending()) : (Sort.by(sortBy).ascending());
 
         PageRequest pages = PageRequest.of(pageNumber, pageSize, sort);
         Page<Product> byTitleContaining = productRepository.findByTitleContaining(pages, keyword);
         PageableResponse<ProductDto> pageableResponse = Helper.getPageableResponse(byTitleContaining, ProductDto.class);
+        log.info("Entering the dao call for find products by title with the pagination");
         return pageableResponse;
     }
 
     @Override
     public ProductDto createProductWithCategory(ProductDto productDto, String categoryId) {
+        log.info("Entering the dao call for create product with category with categoryId:{}",categoryId);
         Category category = this.categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFound(AppConstant.NOT_FOUND + categoryId));
         Product product = this.modelMapper.map(productDto, Product.class);
 
@@ -129,26 +145,31 @@ public class ProductServiceImpl implements ProductService {
         product.setAddedDate(date);
         product.setCategory(category);
         Product product1 = this.productRepository.save(product);
+        log.info("Completed the dao call for create product with category with categoryId:{}",categoryId);
         return modelMapper.map(product1,ProductDto.class);
     }
 
     @Override
     public PageableResponse<ProductDto> getAllOfCategory(String categoryId, Integer pageNumber, Integer pageSize, String sortBy, String sortDir) {
+        log.info("Entering the dao call for get all of products with categoty with category with categoryId:{}",categoryId);
         Category category = this.categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFound(AppConstant.NOT_FOUND + categoryId));
         Sort sort = (sortDir.equalsIgnoreCase("desc")) ? (Sort.by(sortBy).descending()) : (Sort.by(sortBy).ascending());
         PageRequest pages = PageRequest.of(pageNumber, pageSize, sort);
         Page<Product> byCategories = this.productRepository.findByCategory(category, pages);
         PageableResponse<ProductDto> pageableResponse = Helper.getPageableResponse(byCategories, ProductDto.class);
+        log.info("Completed the dao call for get all of products with categoty with category with categoryId:{}",categoryId);
         return pageableResponse;
     }
 
     @Override
     public ProductDto updateCategory(String productId, String categoryId) {
+        log.info("Entering the dao call for update product with categoty with categoryId and productId:{}:{}",categoryId,productId);
         Category category = this.categoryRepository.findById(categoryId).orElseThrow(() -> new ResourceNotFound(AppConstant.NOT_FOUND + categoryId));
         Product product = this.productRepository.findById(productId).orElseThrow(() -> new ResourceNotFound(AppConstant.NOT_FOUND + productId));
         product.setCategory(category);
         Product save = this.productRepository.save(product);
         ProductDto dto = modelMapper.map(save, ProductDto.class);
+        log.info("Completed the dao call for update product with categoty with categoryId and productId:{}:{}",categoryId,productId);
         return dto;
     }
 }
